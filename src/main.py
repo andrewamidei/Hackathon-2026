24488f0a91a55de15bceb1c71f1553afcc012b0f
James Shufelt <jamesshufeltiii@gmail.com>
2026-04-04 02:06:56 -0500

    Spotify Updated Changes

--- src/main.py
+++ src/main.py
@@ -3,18 +3,19 @@ import streamlit as st
 
 # Import your custom function from the handler file
 # from spotifyHandler import render_spotify_player
-from spotifyHandler import search_spotify_player, render_spotify_player
+from spotifyHandler import SpotifyHandler
 
 def main():
     
     if 'selected_track' not in st.session_state:
         st.session_state.selected_track = None
+    if 'selected_sp' not in st.session_state:
+        st.session_state.selected_sp = None
 
-
-    search_spotify_player()
+    SpotifyHandler.search_spotify_player()
     st.title("Spotify Player")
     if st.session_state.selected_track:
-        render_spotify_player(st.session_state.selected_track)
+        SpotifyHandler.render_spotify_player(st.session_state.selected_track,st.session_state.selected_sp)
     
 if __name__ == "__main__":
     main()
